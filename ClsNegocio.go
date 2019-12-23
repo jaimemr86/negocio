1d783e0de8ed6c7108bfde597d09d8a6c373a51b
jmorales2 <jmorales@neodata.com.mx>
2019-12-23 09:44:20 -0600

    cambios
    cambios

--- ClsNegocio.go
+++ ClsNegocio.go
@@ -7,7 +7,9 @@ import (
 	"google.golang.org/api/iterator"
 )
 
-func CatalogoRegistraActualiza(ListaCatalogo []clases.ClsCatalogo,client spanner.Client, ctx context.Context) (result clases.ClsRegresaCatalogo) {
+var client *spanner.Client
+
+func CatalogoRegistraActualiza(ListaCatalogo []clases.ClsCatalogo, ctx context.Context) (result clases.ClsRegresaCatalogo) {
 
 	lsDic := make(map[int64]int64)
 	var idCodigo int64
